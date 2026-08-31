19283b32525b8aa645678c1182a0601045015536
Pamu Harish <harishpamu96@gmail.com>
2026-08-31 14:48:22 +0530

    Use Buildroot toolchain for aesdsocket

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -16,7 +16,7 @@ AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
-	$(MAKE) CROSS_COMPILE="$(TARGET_CROSS)" -C $(@D)/server
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server
 endef
 
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
